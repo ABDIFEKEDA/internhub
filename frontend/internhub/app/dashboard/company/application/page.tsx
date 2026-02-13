052db596e23212bc86dcdc70dfc62efbd6452ed2
ABDIFEKEDA <abdifk23@gmail.com.com>
2026-02-13 16:48:11 +0300

    add company side bar

---
+++ frontend/internhub/app/dashboard/company/application/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { CompanySidebar } from '@/components/sidebar/CompanySidebar'
+
+const page = () => {
+  return (
+    <div className="flex">
+      <CompanySidebar />
+
+    </div>
+  )
+}
+
+export default page
